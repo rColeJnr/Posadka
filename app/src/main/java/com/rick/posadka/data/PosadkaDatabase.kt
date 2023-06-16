9d1b642ae576b33db18d844dcc7055141971ead5
rColeJnr <72414394+rColeJnr@users.noreply.github.com>
2023-06-16 20:22:09 +0300

    database code

--- app/src/main/java/com/rick/posadka/data/PosadkaDatabase.kt
+++ app/src/main/java/com/rick/posadka/data/PosadkaDatabase.kt
@@ -1,10 +1,35 @@
 package com.rick.posadka.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.rick.posadka.model.PosadkaHole
 import com.rick.posadka.model.PosadkaShaft
 
 @Database(entities = [PosadkaHole::class, PosadkaShaft::class], version = 1)
-abstract class PosadkaDatabase {
-    abstract fun posadkaDao(): PosadkaHoleDao
+abstract class PosadkaDatabase: RoomDatabase() {
+    abstract fun posadkaHoleDao(): PosadkaHoleDao
+    abstract fun posadkaShaftDao(): PosadkaShaftDao
+
+    companion object {
+        private const val DATABASE_NAME = "posadka_database"
+
+        // For singleton initialization
+        @Volatile private var instance: PosadkaDatabase? = null
+
+        fun getInstance(context: Context): PosadkaDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        // create and pre-populate the db
+        private fun buildDatabase(context: Context): PosadkaDatabase {
+            return Room.databaseBuilder(context, PosadkaDatabase::class.java, DATABASE_NAME)
+                .createFromAsset("database/posadka_database.db")
+                .build()
+        }
+    }
 }
+
