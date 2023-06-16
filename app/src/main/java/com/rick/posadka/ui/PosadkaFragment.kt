b9726b2a82e88a71c304f5118d224a8214fadac9
rColeJnr <72414394+rColeJnr@users.noreply.github.com>
2023-06-16 20:57:34 +0300

    backend done, sort  of.

--- app/src/main/java/com/rick/posadka/ui/PosadkaFragment.kt
+++ app/src/main/java/com/rick/posadka/ui/PosadkaFragment.kt
@@ -5,26 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.rick.posadka.databinding.FragmentMainBinding
+import androidx.fragment.app.viewModels
+import com.rick.posadka.databinding.FragmentPosadkaBinding
+import com.rick.posadka.viewmodel.PosadkaViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PosadkaFragment: Fragment() {
 
-    private var _binding : FragmentMainBinding? = null
+    private var _binding : FragmentPosadkaBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: PosadkaViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentPosadkaBinding.inflate(inflater, container, false)
 
         binding.bindState()
 
         return binding.root
     }
 
-    private fun FragmentMainBinding.bindState() {
+    private fun FragmentPosadkaBinding.bindState() {
 
     }
 
--- app/src/main/java/com/rick/posadka/viewmodel/PosadkaViewModel.kt
+++ app/src/main/java/com/rick/posadka/viewmodel/PosadkaViewModel.kt
@@ -1,9 +1,30 @@
 package com.rick.posadka.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.rick.posadka.data.PosadkaRepository
+import com.rick.posadka.model.PosadkaHole
+import com.rick.posadka.model.PosadkaShaft
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class PosadkaViewModel: ViewModel() {
+@HiltViewModel
+class PosadkaViewModel @Inject internal constructor(
+    private val posadkaRepository: PosadkaRepository
+): ViewModel() {
 
+    private val _posadkaHole: MutableLiveData<PosadkaHole> = MutableLiveData()
+    val posadkaHole: LiveData<PosadkaHole> get() = _posadkaHole
+    private val _posadkaShaft: MutableLiveData<PosadkaShaft> = MutableLiveData()
+    val posadkaShaft: LiveData<PosadkaShaft> get() = _posadkaShaft
 
+    fun getPosadkaHole(size: Int, name: String) {
+        _posadkaHole.value = posadkaRepository.getPosadkaHole(size, name)
+    }
+
+    fun getPosadkaShaft(size: Int, name: String) {
+        _posadkaShaft.value = posadkaRepository.getPosadkaShaft(size, name)
+    }
 
 }
